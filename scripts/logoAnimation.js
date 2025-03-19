5475210960fdd4efbf6902e821f54f8ce1b0a0a4
Wimmer Philipp <wimmer.philipp@gmx.at>
2025-03-19 12:01:04 +0100

    worked on login

--- scripts/logoAnimation.js
+++ scripts/logoAnimation.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const overlay = document.querySelector(".overlay");
-  const animatedLogo = document.querySelector(".logo");
-  const content = document.querySelector(".animation_content");
 
-  animatedLogo.addEventListener("animationend", () => {
-    overlay.style.display = "none"; // Verstecke das Overlay nach der Animation
-    content.style.opacity = "1"; // Zeige den Content
-  });
-});
